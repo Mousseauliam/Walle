8c380e815bf11ebc61b7d59ce39e5b6f4cf76fe0
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-08 17:13:39 +0200

    __

--- Vision/cam.py
+++ Vision/cam.py
@@ -33,9 +33,9 @@ R_eye_history = [0]*5
 
 #body variables
 last_results_pose = None
-last_wrist__L = [0.0]*3
-last_wrist__R = [0.0]*3
-last_elbow__L = [0.0]*3
+last_wrist_L = [0.0]*3
+last_wrist_R = [0.0]*3
+last_elbow_L = [0.0]*3
 last_elbow_R = [0.0]*3
 last_time = time.time()
 velocity = [0]*4
@@ -76,7 +76,7 @@ def gen_frames():
 
 
 def frame_process():
-    global last_frame, last_results, head_detected, blink, L_eye_closed, R_eye_closed, head_tilt_history, x_position_history, y_position_history, last_results_pose, last_wrist__L, last_wrist__R, last_elbow__L, last_elbow__R, last_time, velocity
+    global last_frame, last_results, head_detected, blink, L_eye_closed, R_eye_closed, head_tilt_history, x_position_history, y_position_history, last_results_pose, last_wrist_L, last_wrist_R, last_elbow_L, last_elbow_R, last_time, velocity
     if last_results.multi_face_landmarks:
         head_detected = True
         face_landmarks = last_results.multi_face_landmarks[0]
@@ -126,16 +126,16 @@ def frame_process():
         
         now= time.time()
         
-        velocity[0] = np.sqrt((wrist_L.x - last_wrist__L[0])**2 + (wrist_L.y - last_wrist__L[1])**2 + (wrist_L.z - last_wrist__L[2])**2) / (now - last_time)
-        velocity[1] = np.sqrt((wrist_R.x - last_wrist__R[0])**2 + (wrist_R.y - last_wrist__R[1])**2 + (wrist_R.z - last_wrist__R[2])**2) / (now - last_time)
-        velocity[2] = np.sqrt((elbow_L.x - last_elbow__L[0])**2 + (elbow_L.y - last_elbow__L[1])**2 + (elbow_L.z - last_elbow__L[2])**2) / (now - last_time)
-        velocity[3] = np.sqrt((elbow_R.x - last_elbow__R[0])**2 + (elbow_R.y - last_elbow__R[1])**2 + (elbow_R.z - last_elbow__R[2])**2) / (now - last_time)
+        velocity[0] = np.sqrt((wrist_L.x - last_wrist_L[0])**2 + (wrist_L.y - last_wrist_L[1])**2 + (wrist_L.z - last_wrist_L[2])**2) / (now - last_time)
+        velocity[1] = np.sqrt((wrist_R.x - last_wrist_R[0])**2 + (wrist_R.y - last_wrist_R[1])**2 + (wrist_R.z - last_wrist_R[2])**2) / (now - last_time)
+        velocity[2] = np.sqrt((elbow_L.x - last_elbow_L[0])**2 + (elbow_L.y - last_elbow_L[1])**2 + (elbow_L.z - last_elbow_L[2])**2) / (now - last_time)
+        velocity[3] = np.sqrt((elbow_R.x - last_elbow_R[0])**2 + (elbow_R.y - last_elbow_R[1])**2 + (elbow_R.z - last_elbow_R[2])**2) / (now - last_time)
         
         # wrist position
-        last_wrist__L = [wrist_L.x, wrist_L.y, wrist_L.z]
-        last_wrist__R = [wrist_R.x, wrist_R.y, wrist_R.z]
-        last_elbow__L = [elbow_L.x, elbow_L.y, elbow_L.z]
-        last_elbow__R = [elbow_R.x, elbow_R.y, elbow_R.z]
+        last_wrist_L = [wrist_L.x, wrist_L.y, wrist_L.z]
+        last_wrist_R = [wrist_R.x, wrist_R.y, wrist_R.z]
+        last_elbow_L = [elbow_L.x, elbow_L.y, elbow_L.z]
+        last_elbow_R = [elbow_R.x, elbow_R.y, elbow_R.z]
     
 def get_head_factor():
     if head_detected:
