ae855d9629e69d23b8390cd9a38110c0d6733615
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-10 19:06:30 +0200

    ...

--- Main.py
+++ Main.py
@@ -77,7 +77,6 @@ while power:
             last_state_change = time.time()
             
     if state_btn[2] == 0:
-        """
         print("[Main] sleep ...")
         if not sleep :
             robot.sleep()
@@ -86,8 +85,7 @@ while power:
             robot.wake_up()
             manager.launch_mode(modes[current_mode_name])
         sleep = not sleep
-        time.sleep(0.5)"""
-        robot.sound("waow2")
+        time.sleep(0.5)
         
         
         
