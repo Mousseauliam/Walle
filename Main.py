fe698f65d36bc0ace9a290f230df65f7741f9e5c
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-10 15:39:42 +0200

    ...

--- Main.py
+++ Main.py
@@ -87,7 +87,7 @@ while power:
             manager.launch_mode(modes[current_mode_name])
         sleep = not sleep
         time.sleep(0.5)"""
-        robot.sound("oh1_cleaned")
+        robot.sound("walle_oh_synth")
         
         
         
