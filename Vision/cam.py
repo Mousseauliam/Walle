0f0432fb493586ea5e75f5716b0f3ed52ddebc1f
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-11 15:30:17 +0200

    ...

--- Vision/cam.py
+++ Vision/cam.py
@@ -255,7 +255,7 @@ def get_factor():
         
         blink_type = "none"
         brown_type = "none"
-        if all(v < eye_look_threshold for v in eye_look_values.values())
+        if all(v < eye_look_threshold for v in eye_look_values.values()):
             
             #blink detection
             if (L_eye_ratio > blink_threshold) and (R_eye_ratio > blink_threshold_R):
