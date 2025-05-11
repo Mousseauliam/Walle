83ad5a6d37c058d792f85b0589aa0f2fe54ea34e
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-11 21:02:42 +0200

    ...

--- Modes/Auto.py
+++ Modes/Auto.py
@@ -76,7 +76,7 @@ def run(robot,server):
             if ((time.time() - last_mvt) > next_random):
                 print('maintenant')
                 robot.neckAngle(round(max(0, min(robot.get_coef('neck_angle') + random.uniform(-0.1, 0.1), 0.7)),2))
-                print(round(max(0,min(robot.get_coef('neck_LR') + random.uniform(-0.2, 0.2)), 1),2))
+                print(robot.get_coef('neck_LR') + random.uniform(-0.2, 0.2))
                 #robot.neckLR()
                 last_mvt = time.time()
                 print(f"réinitialisation tps {last_mvt}")
