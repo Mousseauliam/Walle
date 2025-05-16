2e4d803e0b2f7eea4e030ceadddfadf73f6f6e98
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-16 11:01:42 +0200

    follow

--- Modes/Follow.py
+++ Modes/Follow.py
@@ -15,7 +15,8 @@ def run(robot,server):
     y_step = 0.01
     while active:
         factor=Follow_factor()
-
+        print(z_threshold)
+        '''
         if factor[1]>(z_threshold+z_deadzone) :
             robot.move(0.5+speed)
         
@@ -24,7 +25,7 @@ def run(robot,server):
         
         else:
             robot.move(0.5)
-        
+        '''
         neck_angle = robot.get_coef("neck_angle")
         if (factor[2] < (0.5 - deadzone)) and (neck_angle>y_step) :
             robot.neckAngle(round(neck_angle - y_step,2))
