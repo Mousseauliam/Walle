ae267f0b7b171ad2be6ccfb258589b336cefc3fe
Noahnanas <noah.milakovic23@gmail.com>
2025-05-16 12:40:27 +0200

    ...

--- Services/Mvt_walle.py
+++ Services/Mvt_walle.py
@@ -134,8 +134,8 @@ class Walle:
             self.coef["neck_L"]=self.coef["neck_L"]+ratio
             self.coef["neck_U"]=self.coef["neck_U"]+ratio
         elif neckLevel=0.5:
-            self.coef["neck_L"]=self.coef["neck_L"]/2
-            self.coef["neck_L"]=self.coef["neck_L"]/2
+            self.coef["neck_L"]==self.coef["neck_L"]/2
+            self.coef["neck_U"]==self.coef["neck_L"]/2
 
         if self.coef["neck_L"]>1:
             a=self.coef["neck_L"]-1
